fa8f23d475cf2724bbaaf16e15e8945f4684aa82
Ajmal Nasumudeen <ajmaln73@gmail.com>
2021-11-24 10:16:49 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# HR Management System and Timesheet" 
---
+++ server/index.js
@@ -0,0 +1,39 @@
+import createError from 'http-errors';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from 'cors'
+import employeeRouter from './routes/employee.js';
+import hrRouter from './routes/hr.js';
+
+const app = express();
+const CONNECTION_URL = "mongodb://localhost:27017/hr-management";
+const port = 4000;
+
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.use('/', employeeRouter);
+app.use('/hr', hrRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(port, ()=> console.log("server running on port " + port))
